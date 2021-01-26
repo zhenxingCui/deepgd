3881e28049add39b684d4c403b5b3b68f03fb13a
Xiaoqi Wang <xiaoqiwang2582@gmail.com>
2021-01-26 20:32:46 +0000

    update

--- lib/data.py
+++ lib/data.py
@@ -49,7 +49,7 @@ def load_G_list(*, data_path=None, index_file=None, data_slice=slice(None)):
 
 
 @cache
-def generate_data_list(G, *, sparse=False, pivot_mode='random', device='cpu'):
+def generate_data_list(G, *, sparse=False, k_fn=None, pivot_mode='random', device='cpu'):
     def generate_pivots(G, apsp, k=None, mode='random'):
         def generate_random_pivots(G, apsp, k):
             return random.sample(list(G.nodes), k)
@@ -141,7 +141,7 @@ def generate_data_list(G, *, sparse=False, pivot_mode='random', device='cpu'):
         return torch.rand(G.number_of_nodes(), 2)
     
     if type(G) is list:
-        return [generate_data_list(g, sparse=sparse, pivot_mode=pivot_mode, device=device)
+        return [generate_data_list(g, sparse=sparse, k_fn=k_fn, pivot_mode=pivot_mode, device=device)
                 for g in tqdm(G, desc='preprocess G')]
     apsp = generate_apsp(G)
     full_elist = generate_full_edge_list(G)
@@ -151,7 +151,7 @@ def generate_data_list(G, *, sparse=False, pivot_mode='random', device='cpu'):
                 full_edge_index=torch.tensor(full_elist, dtype=torch.long, device=device).t(), 
                 full_edge_attr=torch.tensor(full_eattr, dtype=torch.float, device=device))
     if sparse:
-        k = int(np.round(np.sqrt(G.number_of_nodes())))
+        k = int(np.round(np.sqrt(G.number_of_nodes()))) if k_fn is None else k_fn(G)
         pivots = generate_pivots(G, apsp, k, mode=pivot_mode)
         sparse_elist = generate_sparse_edge_list(G, pivots)
         sparse_eattr = generate_sparse_edge_attr(G, sparse_elist, apsp, pivots)
