194a4090ea241d9747e785d5d2c10652b7cb9c20
Xiaoqi Wang <xiaoqiwang2582@gmail.com>
2021-01-27 10:10:19 +0000

    update

--- lib/functions.py
+++ lib/functions.py
@@ -22,11 +22,13 @@ def l2_normalize(x, return_norm=False, eps=1e-5):
 def get_full_edge_index(batch):
     return batch.full_edge_index.T
 
+
 def get_sparse_edge_index(batch):
     return batch.sparse_edge_index.T
     
+    
 def get_real_edge_index(batch):
-    l = batch.edge_attr[:, 0]
+    l = batch.full_edge_attr[:, 0]
     return get_full_edge_index(batch)[l == l.min()]
 
 
