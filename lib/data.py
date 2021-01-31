e91648dc8196862830c84a24cc8be7faedef916a
Xiaoqi Wang <xiaoqiwang2582@gmail.com>
2021-01-31 03:48:05 +0000

    update data list

--- lib/data.py
+++ lib/data.py
@@ -176,10 +176,10 @@ def generate_data_list(G, *,
     apsp = generate_apsp(G)
     full_elist = generate_full_edge_list(G)
     full_eattr = generate_regular_edge_attr(G, full_elist, apsp)
-    x = generate_initial_node_attr(G, mode=init_mode).to(device) if init_mode is not None else None
-    data = Data(x=x, 
-                full_edge_index=torch.tensor(full_elist, dtype=torch.long, device=device).t(), 
+    data = Data(full_edge_index=torch.tensor(full_elist, dtype=torch.long, device=device).t(), 
                 full_edge_attr=torch.tensor(full_eattr, dtype=torch.float, device=device))
+    if if init_mode is not None:
+        data.x = generate_initial_node_attr(G, mode=init_mode).to(device)
     if sparse:
         if sparse == 'sqrt':
             k = np.round(np.sqrt(n))
