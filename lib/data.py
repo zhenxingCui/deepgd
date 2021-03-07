d7e8d79beb27dd80ce824f293a62d6b44ae5935a
Xiaoqi Wang <xiaoqiwang2582@gmail.com>
2021-03-07 08:32:41 -0500

    fix

--- lib/data.py
+++ lib/data.py
@@ -489,7 +489,7 @@ def generate_data_list(G, *,
 
 def prepare_discriminator_data(data, pos=None):
     data.edge_index = data.raw_edge_index
-    data.pos = data.gt_pos is pos is None else pos
+    data.pos = data.gt_pos if pos is None else pos
 
 
 class LazyDeviceMappingDataLoader:
