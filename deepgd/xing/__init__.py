d1915f8750b43deb40a4ae700cece177e5cb06b2
Xiaoqi Wang <xiaoqiwang2582@gmail.com>
2021-09-08 04:35:38 -0400

    refactor xing as nn.Module

--- deepgd/xing/__init__.py
+++ deepgd/xing/__init__.py
@@ -19,63 +19,74 @@ from .._dependencies import *
 #     intersections = segments_intersections(to_seg_list(offset_segments))
 #     return list(set(intersections.keys()))
 
-
-def get_num_xing(pos, batch, eps=1e-5, return_xing=False):
-    def x(v, u):
+@dataclass
+class Xing(nn.Module):
+    eps: float = 1e-5
+    scatter: bool = True, 
+    reduce: None = torch.sum
+    
+    def __post_init__(self):
+        super().__init__()
+        
+    @staticmethod
+    def _x(v, u):
         return torch.cross(
             F.pad(v, (0, 1)),
             F.pad(u, (0, 1))
         )[:, -1]
-
-    def dot(v, u):
-        return (v * u).sum(dim=-1)
-
-    # get pqrs
-    (s1, e1, s2, e2) = batch.edge_pair_index
-    p, q = pos[s1], pos[s2]
-    r, s = pos[e1] - p, pos[e2] - q
-
-    # shrink by eps
-    p += eps * r
-    q += eps * s
-    r *= 1 - 2*eps
-    s *= 1 - 2*eps
-
-    # get intersection
-    qmp = q - p
-    qmpxs = x(qmp, s)
-    qmpxr = x(qmp, r)
-    rxs = x(r, s)
-    rdr = dot(r, r)
-    t = qmpxs / rxs
-    u = qmpxr / rxs
-    t0 = dot(qmp, r) / rdr
-    t1 = t0 + dot(s, r) / rdr
-
-    # calculate bool
-    zero = torch.zeros_like(rxs)
-    parallel = rxs.isclose(zero)
-    nonparallel = parallel.logical_not()
-    collinear = parallel.logical_and(qmpxr.isclose(zero))
-
-    xing = torch.logical_or(
-        collinear.logical_and(
-            torch.logical_and(
-                (t0 > 0).logical_or(t1 > 0),
-                (t0 < 1).logical_or(t1 < 1),
-            )
-        ),
-        nonparallel.logical_and(
-            torch.logical_and(
-                (0 < t).logical_and(t < 1),
-                (0 < u).logical_and(u < 1),
-            )
-        )
-    )
-
-    batch_idx = batch.batch[s1]
-    n_xing = torch_scatter.scatter(xing.float(), batch_idx)
     
-    return xing if return_xing else n_xing
+    @staticmethod
+    def _dot(v, u):
+        return (v * u).sum(dim=-1)
+    
+    def forward(self, pos, batch):
+        # get pqrs
+        (s1, e1, s2, e2) = batch.edge_pair_index
+        p, q = pos[s1], pos[s2]
+        r, s = pos[e1] - p, pos[e2] - q
+
+        # shrink by eps
+        p += self.eps * r
+        q += self.eps * s
+        r *= 1 - 2*self.eps
+        s *= 1 - 2*self.eps
+
+        # get intersection
+        qmp = q - p
+        qmpxs = self._x(qmp, s)
+        qmpxr = self._x(qmp, r)
+        rxs = self._x(r, s)
+        rdr = self._dot(r, r)
+        t = qmpxs / rxs
+        u = qmpxr / rxs
+        t0 = self._dot(qmp, r) / rdr
+        t1 = t0 + self._dot(s, r) / rdr
+
+        # calculate bool
+        zero = torch.zeros_like(rxs)
+        parallel = rxs.isclose(zero)
+        nonparallel = parallel.logical_not()
+        collinear = parallel.logical_and(qmpxr.isclose(zero))
+
+        xing = torch.logical_or(
+            collinear.logical_and(
+                torch.logical_and(
+                    (t0 > 0).logical_or(t1 > 0),
+                    (t0 < 1).logical_or(t1 < 1),
+                )
+            ),
+            nonparallel.logical_and(
+                torch.logical_and(
+                    (0 < t).logical_and(t < 1),
+                    (0 < u).logical_and(u < 1),
+                )
+            )
+        ).float()
+
+        if self.scatter:
+            batch_idx = batch.batch[s1]
+            xing = torch_scatter.scatter(xing, batch_idx)
+
+        return xing if self.reduce is None else self.reduce(xing)
 
     
